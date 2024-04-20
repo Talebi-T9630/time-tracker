222c178c3f4f7d9c25ca14c643b2d30cd4cdccf8
Tarane-Konect <tarane.t@konect.ai>
2024-04-19 18:19:59 -0700

    genrated  users controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+// import { UsersController } from './users/users.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      //TO DO:: ADD Auto-load entities
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'local_db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  user_id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  user_email: string;
+
+  @Column()
+  user_password: string;
+
+  @Column()
+  user_company_id: number;
+
+  @Column({ default: false })
+  user_status: boolean;
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+}
